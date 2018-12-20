f1f7d32e4865284211fa6eda49fc66ce7c2dbfa5
James Dyckowski <jdyver@gmail.com>
2018-12-20 15:06:49 -0500

    JD-GIT AUTO: testing JD's GITPUSH

--- JD_GITPUSH.sh
+++ JD_GITPUSH.sh
@@ -1,5 +1,7 @@
+echo " JD-GIT: Message is $1 $2 $3 $4 $5"
+
 git add -A
-git commit -am "JD-GIT AUTO: $1"
+git commit -am "JD-GIT AUTO: $1 $2 $3 $4 $5"
 git push
 
 echo " JD-GIT: This was pushed to:"
